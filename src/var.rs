38bc5faf6e4e97ccaf9330c802a827d9a02c99d1
adhoc <postma.marin@protonmail.com>
2024-12-31 14:48:31 +0100

    var definition and assign

---
+++ src/var.rs
@@ -0,0 +1,60 @@
+use std::marker::PhantomData;
+
+use cranelift_frontend::Variable;
+use cranelift::prelude::*;
+
+use crate::types::{AsVal, ToJitPrimitive, Val};
+use crate::func::{with_ctx, FnCtx, IntoParams};
+
+#[derive(Copy, Clone)]
+pub struct Var<T> {
+    variable: Variable,
+    _pth: PhantomData<T>,
+}
+
+impl<T> Var<T> {
+    pub fn new(v: T) -> Self
+    where T: ToJitPrimitive
+    {
+        with_ctx(|ctx| {
+            let var = ctx.declare_var();
+            ctx.builder().declare_var(var, T::ty());
+            let val = ctx.builder().ins().iconst(T::ty(), v.to_i64());
+            ctx.builder().def_var(var, val);
+            Self::from_variable(var)
+        })
+    }
+
+    pub fn assign(&mut self, val: impl AsVal<Ty = T>)
+    {
+        with_ctx(|ctx| {
+            let value = val.as_val(ctx);
+            ctx.builder().def_var(self.variable(), value.value());
+        })
+    }
+
+    pub(crate) fn from_variable(variable: Variable) -> Self {
+        Self { variable, _pth: PhantomData }
+    }
+
+    pub(crate) fn variable(&self) -> Variable {
+        self.variable
+    }
+}
+
+impl<T> AsVal for Var<T> {
+    type Ty = T;
+    fn as_val(&self, ctx: &mut FnCtx) -> Val<T> {
+        let val = ctx.builder.use_var(self.variable);
+        Val::new(val)
+    }
+}
+
+impl<T> IntoParams for Var<T> {
+    type Input = T;
+
+    fn params(&self, ctx: &mut FnCtx, out: &mut Vec<Value>) {
+        let val = ctx.builder.use_var(self.variable());
+        out.push(val);
+    }
+}
